626acf24fe53e02b4d92101e7169b26bc65809e8
Elango Balusamy <elango@welab.co>
2013-09-08 20:02:02 +0530

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twistrecipes::Application.config.secret_key_base = 'f64d7df88fcb4aef7e3a74204091c9417eed7d1d1f452d7648cb4c9d1f058a48f90575facaf8f0195eae440646a5768b2cc1cf0ec7fd609fcfcafc8b68487216'
